7ba605ee4ed668b7b5db4bd0212be0bc954df65d
1d3a portatil - Kenneth Suarez <kenneth.suarez@1d3a.com>
2020-08-21 01:55:32 +0200

    Update server.js

--- express/server.js
+++ express/server.js
@@ -11,19 +11,19 @@ const engine = require('ejs-mate');
 
 
 const router = express.Router();
+app.set('views', 'views');
+app.engine('ejs', engine);
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
   // res.write('<h1>Hello from Express.js!</h1>');
-  res.render('ejs/ArtiMotor/angryShark.ejs');
+  res.render('ejs/ArtiMotor/workInProgress.ejs');
 
-  res.end();
+  // res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 
-app.set('views', 'views');
-app.engine('ejs', engine);
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
